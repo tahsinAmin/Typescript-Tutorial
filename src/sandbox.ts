dfa4e3c370325a4fe9bd66d65d58711ee54b6bbb
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-07-22 12:31:51 +0600

    TypeScript Tutorial #10 - Function Signatures

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -66,3 +66,34 @@
 // const greetAgain = (user: objWithName) => {
 //   console.log(`${user.name} says hello`);
 // }
+
+// ----------TypeScript Tutorial #10 - Function Signatures---------
+// Function signature descrbes the general definition of a function,
+//  what argument it takes in and what type it returns.
+
+// let greet : Function
+
+// example 1
+let greet: (a: string, b: string) => void // It's hgoing to retr me void.
+
+greet = (name: string, greetings: string) => {
+  console.log(`${name} says ${greetings}`);
+}
+
+// example 2
+let calc: (a: number, b: number, c: string) => number;
+
+calc = (numOne: number, numTwo: number, action: string) => {
+  if(action === 'add'){
+    return numOne + numTwo
+  }else {
+    return numOne - numTwo
+  }
+}
+
+// example 3
+let logDetails: (obj: {name:string, age: number})=> void;
+logDetails = (ninja: {name:string, age: number} ) => {
+console.log(`${ninja.name} is a ${ninja.age} years old`);
+
+}
