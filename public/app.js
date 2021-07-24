69530185753e55cc1d4502ee2c0f83acd40eef1f
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-07-24 12:10:32 +0600

    TypeScript Tutorial #12 - Classes

--- public/app.js
+++ public/app.js
@@ -1,13 +1,49 @@
 "use strict";
-// When you give ! at the end, then you're sure that there's an element
-var anchor = document.querySelector('a');
-// if(anchor){
-//   console.log(anchor.href);
-// }
-console.log(anchor.href);
-// What if there's another form?
-// const form = document.querySelector('form')!;
-// Cast it to HTMLFormElement
+// // ----------TypeScript Tutorial #11 - DOM & Typecasting----------
+// // When you give ! at the end, then you're sure that there's an element
+// const anchor = document.querySelector('a')!;
+// // if(anchor){
+// //   console.log(anchor.href);
+// // }
+// console.log(anchor.href);
+// // What if there's another form?
+// // const form = document.querySelector('form')!;
+// // Cast it to HTMLFormElement
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.element);
+// // inputs
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+// form.addEventListener('submit', (e:Event) => {
+//   e.preventDefault();
+//   console.log(
+//     type.value,
+//     tofrom.value,
+//     details.value,
+//     amount.valueAsNumber
+//   );
+// });
+// ----------TypeScript Tutorial #12 - Classes---------
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Mario', 'work on the Mario website', 250);
+var invTwo = new Invoice('Luigi', 'work on the Mario website', 300);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
 var form = document.querySelector('.new-item-form');
 console.log(form.element);
 // inputs
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,66 @@
-// When you give ! at the end, then you're sure that there's an element
-const anchor = document.querySelector('a')!;
+// // ----------TypeScript Tutorial #11 - DOM & Typecasting----------
+// // When you give ! at the end, then you're sure that there's an element
+// const anchor = document.querySelector('a')!;
 
-// if(anchor){
-//   console.log(anchor.href);
-// }
-console.log(anchor.href);
+// // if(anchor){
+// //   console.log(anchor.href);
+// // }
+// console.log(anchor.href);
 
-// What if there's another form?
-// const form = document.querySelector('form')!;
+// // What if there's another form?
+// // const form = document.querySelector('form')!;
 
-// Cast it to HTMLFormElement
+// // Cast it to HTMLFormElement
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.element);
+
+// // inputs
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// form.addEventListener('submit', (e:Event) => {
+//   e.preventDefault();
+//   console.log(
+//     type.value,
+//     tofrom.value,
+//     details.value,
+//     amount.valueAsNumber
+//   );
+// });
+
+// ----------TypeScript Tutorial #12 - Classes---------
+
+// classes
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c:string, d: string, a:number){
+    this.client=c;
+    this.details=d;
+    this.amount=a;
+  }
+
+  format(){
+    return `${this.client} owes $${this.amount} for ${this.details}`
+  }
+}
+
+const invOne = new Invoice('Mario','work on the Mario website',250);
+const invTwo = new Invoice('Luigi','work on the Mario website',300);
+
+const invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+console.log(invoices);
+
+
+
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.element);
 
@@ -21,6 +72,7 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e:Event) => {
   e.preventDefault();
+
   console.log(
     type.value,
     tofrom.value,
