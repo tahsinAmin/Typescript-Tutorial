fb3c54689102211bffe5bacdbf022275d1eaadd7
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-07-25 18:48:30 +0600

    TypeScript Tutorial #13 - Public, Private & Readonly

--- public/app.js
+++ public/app.js
@@ -25,27 +25,70 @@
 //     amount.valueAsNumber
 //   );
 // });
-// ----------TypeScript Tutorial #12 - Classes---------
+// // ----------TypeScript Tutorial #12 - Classes---------
+// // classes
+// class Invoice {
+//   client: string;
+//   details: string;
+//   amount: number;
+//   constructor(c:string, d: string, a:number){
+//     this.client=c;
+//     this.details=d;
+//     this.amount=a;
+//   }
+//   format(){
+//     return `${this.client} owes $${this.amount} for ${this.details}`
+//   }
+// }
+// const invOne = new Invoice('Mario','work on the Mario website',250);
+// const invTwo = new Invoice('Luigi','work on the Mario website',300);
+// const invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// console.log(invoices);
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.element);
+// // inputs
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+// form.addEventListener('submit', (e:Event) => {
+//   e.preventDefault();
+//   console.log(
+//     type.value,
+//     tofrom.value,
+//     details.value,
+//     amount.valueAsNumber
+//   );
+// });
+// ----------TypeScript Tutorial #13 - Public, Private & Readonly---------
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    function Invoice(// if use this way then we have to give the access modifiers
+    client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
-        return this.client + " owes $" + this.amount + " for " + this.details;
+        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
     };
     return Invoice;
 }());
-var invOne = new Invoice('Mario', 'work on the Mario website', 250);
-var invTwo = new Invoice('Luigi', 'work on the Mario website', 300);
+var invOne = new Invoice('mario', 'work on the mario website', 250);
+var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 var invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-console.log(invoices);
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.amount, inv.format());
+});
 var form = document.querySelector('.new-item-form');
-console.log(form.element);
+console.log(form.children);
 // inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
--- src/app.ts
+++ src/app.ts
@@ -30,53 +30,100 @@
 //   );
 // });
 
-// ----------TypeScript Tutorial #12 - Classes---------
+// // ----------TypeScript Tutorial #12 - Classes---------
 
+// // classes
+// class Invoice {
+//   client: string;
+//   details: string;
+//   amount: number;
+
+//   constructor(c:string, d: string, a:number){
+//     this.client=c;
+//     this.details=d;
+//     this.amount=a;
+//   }
+
+//   format(){
+//     return `${this.client} owes $${this.amount} for ${this.details}`
+//   }
+// }
+
+// const invOne = new Invoice('Mario','work on the Mario website',250);
+// const invTwo = new Invoice('Luigi','work on the Mario website',300);
+
+// const invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// console.log(invoices);
+
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// console.log(form.element);
+
+// // inputs
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// form.addEventListener('submit', (e:Event) => {
+//   e.preventDefault();
+
+//   console.log(
+//     type.value,
+//     tofrom.value,
+//     details.value,
+//     amount.valueAsNumber
+//   );
+// });
+
+// ----------TypeScript Tutorial #13 - Public, Private & Readonly---------
 // classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
+  // readonly client: string;
+  // private details: string;
+  // public amount: number;
 
-  constructor(c:string, d: string, a:number){
-    this.client=c;
-    this.details=d;
-    this.amount=a;
-  }
+  constructor( // if use this way then we have to give the access modifiers
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ){}
 
-  format(){
-    return `${this.client} owes $${this.amount} for ${this.details}`
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
   }
 }
 
-const invOne = new Invoice('Mario','work on the Mario website',250);
-const invTwo = new Invoice('Luigi','work on the Mario website',300);
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-const invoices: Invoice[] = [];
-invoices.push(invOne);
+let invoices: Invoice[] = [];
+invoices.push(invOne)
 invoices.push(invTwo);
 
-console.log(invoices);
-
-
+invoices.forEach(inv => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.element);
+console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
+const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
-form.addEventListener('submit', (e:Event) => {
+form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
   console.log(
-    type.value,
-    tofrom.value,
-    details.value,
+    type.value, 
+    tofrom.value, 
+    details.value, 
     amount.valueAsNumber
   );
-});
+}); 
