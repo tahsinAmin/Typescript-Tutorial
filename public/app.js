1e83c653cb4c15ee0ca8381977dd68908a6456c5
tahsinAmin <muhammadtahsinamin@gmail.com>
2021-08-01 15:26:12 +0600

    TypeScript Tutorial #20 - Tuples

--- public/app.js
+++ public/app.js
@@ -13,30 +13,49 @@ const ul = document.querySelector('ul');
 const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
 });
-// GENERICS
-const addUID = (obj) => {
-    let uid = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { uid });
+// // GENERICS
+// const addUID = <T extends {name:string}>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+var ResourceType;
+(function (ResourceType) {
+    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
+    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
+    ResourceType[ResourceType["FILM"] = 2] = "FILM";
+    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
+    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
+})(ResourceType || (ResourceType = {}));
+const docOne = {
+    uid: 1,
+    resourceType: ResourceType.BOOK,
+    data: { title: 'nam of the wind' }
 };
-let docOne = addUID({ name: 'yoshi', age: 40 });
-console.log(docOne.name);
-const docThree = {
-    uid: 24,
-    resourceName: 'person',
-    data: { name: 'shaun' }
+const docTwo = {
+    uid: 10,
+    resourceType: ResourceType.PERSON,
+    data: { name: 'Yoshi' }
 };
-const docFour = {
-    uid: 24,
-    resourceName: 'shoppingList',
-    data: ['bread', 'milk']
-};
-console.log(docThree, docFour);
+console.log(docOne, docTwo);
+// Tuples = thje types of data in each position in a tuple is fixed once it's intialised
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi'];
+let tup = ['ryu', 25, false];
+tup[0] = 'ken';
+tup[1] = 24;
+// let student: [string,number]
+// student = [2342,'asd']
+// student = ['josh', 345]
--- src/app.ts
+++ src/app.ts
@@ -19,45 +19,62 @@ const list = new ListTemplate(ul);
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
+let values:[string, string, number];
+values = [tofrom.value, details.value, amount.valueAsNumber];
+
   let doc: HasFormatter;
   if (type.value === 'invoice') {
-    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Invoice(...values);
   } else {
-    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    doc = new Payment(...values);
   }
   
   list.render(doc, type.value, 'end');
 });
 
-// GENERICS
 
-const addUID = <T extends {name:string}>(obj: T) => {
-  let uid = Math.floor(Math.random() * 100);
-  return {...obj, uid};
-}
 
-let docOne = addUID({name: 'yoshi', age: 40});
+// // GENERICS
 
-console.log(docOne.name);
+// const addUID = <T extends {name:string}>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+
+enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
 
 // interface defines how an object should look
 interface Resource<T> {
   uid: number;
-  resourceName: string;
+  resourceType: ResourceType;
   data: T;
 }
 
-const docThree: Resource<object> = {
-  uid:24,
-  resourceName: 'person',
-  data:{name: 'shaun'}
+const docOne: Resource<object> = {
+  uid:1,
+  resourceType: ResourceType.BOOK,
+  data:{title: 'nam of the wind'}
 }
 
-const docFour: Resource<string[]> = {
-  uid:24,
-  resourceName: 'shoppingList',
-  data:['bread', 'milk']
+const docTwo: Resource<object> = {
+  uid:10,
+  resourceType: ResourceType.PERSON,
+  data:{name: 'Yoshi'}
 }
 
-console.log(docThree, docFour);
+console.log(docOne, docTwo);
 
+// Tuples = thje types of data in each position in a tuple is fixed once it's intialised
+let arr = ['ryu', 25, true]
+arr[0] = false;
+arr[1] = 'yoshi';
+arr = [30, false, 'yoshi']
+
+let tup: [string, number, boolean] = ['ryu', 25, false];
+tup[0] = 'ken'
+tup[1] = 24
+
+// let student: [string,number]
+// student = [2342,'asd']
+// student = ['josh', 345]
